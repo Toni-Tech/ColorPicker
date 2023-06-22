62c7dd100728cdbfb1910688e55d664eefbb42b3
Toni <toni@Clarks-iMac.local>
2023-06-22 16:59:03 -0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-// Write your Color component here
+import { useState } from "react";
 
+const Color = (props) => {
+  return <div className={props.color}></div>
+}
 const App = () => {
   return (
+   
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+      <div id="colors-list">
+        <Color color="red"/>
+        <Color color="blue" />
+        <Color color="yellow" />
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+        
+      </div>
+
     </div>
   );
 };

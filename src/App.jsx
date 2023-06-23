694874beb7fd8247f72fa22d471bfd98647eee42
Toni <toni@Clarks-iMac.local>
2023-06-22 17:13:49 -0700

    Update colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 
-const Color = (props) => {
-  return <div className={props.color}></div>
+const Color = ({color, setSelectedColor}) => {
+return <div className={color} onClick={()=>setSelectedColor(color)}></div>
 }
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("");
   return (
    
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
+        <div className={selectedColor}>{selectedColor}</div>
       <div id="colors-list">
-        <Color color="red"/>
-        <Color color="blue" />
-        <Color color="yellow" />
+        <Color color="red" setSelectedColor={setSelectedColor}/>
+        <Color color="blue" setSelectedColor={setSelectedColor}/>
+        <Color color="yellow" setSelectedColor={setSelectedColor}/>
       </div>
         
       </div>
